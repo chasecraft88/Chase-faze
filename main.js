44a3353e38e4b82cf2ca2d5ca844adb0d2e2764f
Chase <112102369+chasecraft88@users.noreply.github.com>
2023-11-03 20:00:36 +0000

    Update main.js

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ let player;
 let zombies;
 
 function preload() {
-    this.load.image('player', 'assets/player.png');
-    this.load.image('zombie', 'assets/zombie.png');
+    this.load.image('player', 'assets/assets/Img3.Png');
+    this.load.image('zombie', 'assets/assets/Img2.Png');
 }
 
 function create() {
